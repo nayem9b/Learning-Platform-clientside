ac4bee7c04511c4305374c5cee7abdb664a22da3
nayem9b <razwanniam1@gmail.com>
2022-10-24 22:34:55 +0600

    login and reg page partially fixed

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1PPccPYfc4SheM_5XHvssARnu4yvTBRA",
+  authDomain: "web-smasher.firebaseapp.com",
+  projectId: "web-smasher",
+  storageBucket: "web-smasher.appspot.com",
+  messagingSenderId: "757409987787",
+  appId: "1:757409987787:web:e4eadc4b4983bbe4632f4e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
