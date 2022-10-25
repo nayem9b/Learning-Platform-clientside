fe50f60c2b0be852d01c1de51170b892fec82177
nayem9b <razwanniam1@gmail.com>
2022-10-25 06:27:33 +0600

    card section desciption added

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,21 +1,29 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ crdt, setDetails, details }) => {
+  const { logo, name, total, id } = crdt;
+  //   console.log(crdt);
+  //   console.log(name);
+  const handleButtonClick = () => {
+    setDetails(crdt.name);
+    console.log(details);
+    // console.log(crdt.name, crdt.total, crdt.id);
+  };
   return (
     <div>
-      <a href='#' class='group relative block bg-black'>
+      <button onClick={handleButtonClick} class='group relative block bg-black'>
         <img
           alt='Developer'
-          src='https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80'
+          src={logo}
           class='absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50'
         />
 
         <div class='relative p-8'>
           <p class='text-sm font-medium uppercase tracking-widest text-pink-500'>
-            Developer
+            Course
           </p>
 
-          <p class='text-2xl font-bold text-white'>Tony Wayne</p>
+          <p class='text-2xl font-bold text-white'>{crdt.name}</p>
 
           <div class='mt-64'>
             <div class='translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100'>
@@ -27,7 +35,7 @@ const Card = () => {
             </div>
           </div>
         </div>
-      </a>
+      </button>
     </div>
   );
 };
--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Card from "../Card/Card";
 
 const Courses = () => {
   const loaderData = useLoaderData();
-  const QuizData = loaderData;
+  const CourseData = loaderData;
+  const [details, setDetails] = useState("");
   return (
     <div>
       <div className='flex '>
-        <h1 className='w-1/4'>hello</h1>
+        <h1 className='w-1/4'>{details}</h1>
         <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  w-3/4 justify-center items-center'>
-          {QuizData.map((quizdt) => (
-            <Card key={quizdt.id} quizdt={quizdt}>
-              {" "}
-              hello
-            </Card>
+          {CourseData.map((crdt) => (
+            <Card
+              key={crdt.id}
+              crdt={crdt}
+              details={details}
+              setDetails={setDetails}></Card>
           ))}
         </div>{" "}
       </div>
